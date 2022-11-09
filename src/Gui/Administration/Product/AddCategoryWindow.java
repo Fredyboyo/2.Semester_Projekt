b794fd8032fd208b0c5b1953e7e6d152b1c0d438
Fredyboyo <83766714+Fredyboyo@users.noreply.github.com>
2022-11-09 13:27:43 +0100

    +

--- src/Gui/Administration/Product/AddCategoryWindow.java
+++ src/Gui/Administration/Product/AddCategoryWindow.java
@@ -4,16 +4,18 @@ import Controller.Controller;
 import Model.Category;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class AddCategoryWindow extends Stage {
-    private final TextField tfName = new TextField();
-    private final TextField tfMortgage = new TextField();
-    private Category category;
+
+    private final TextField txfName = new TextField();
 
     public AddCategoryWindow(){
         this.initStyle(StageStyle.UTILITY);
@@ -30,59 +32,31 @@ public class AddCategoryWindow extends Stage {
         this.setScene(scene);
     }
 
-    private void initContent(GridPane gridPane) {
-        gridPane.setPadding(new Insets(25));
-        gridPane.setVgap(10);
-        gridPane.setHgap(10);
+    private void initContent(GridPane pane) {
+        pane.setPadding(new Insets(25));
+        pane.setVgap(10);
+        pane.setHgap(10);
 
-        Label lName = new Label("produktkategori: ");
-        RadioButton rbPant = new RadioButton("Pant: ");
-        Button bOk = new Button("Bekræft");
+        Label lblName = new Label("Produktkategori");
+        pane.add(lblName, 1, 1);
 
-        tfMortgage.setDisable(true);
+        pane.add(txfName, 2, 1);
 
-        gridPane.add(lName, 1, 1);
-        gridPane.add(tfName, 2, 1);
-        gridPane.add(rbPant, 1, 2);
-        gridPane.add(tfMortgage, 2, 2);
-        gridPane.add(bOk, 3, 2);
-
-        rbPant.setOnAction(event -> pantAction(rbPant));
-        bOk.setOnAction(event -> okAction());
+        Button btnOK = new Button("Bekræft");
+        pane.add(btnOK, 3, 1);
+        btnOK.setOnAction(event -> okAction());
     }
-
-    private void pantAction(RadioButton bPant) {
-        tfMortgage.setDisable(!bPant.isSelected());
-    }
-
 
     private void okAction() {
-        String name = tfName.getText();
-        if(name.isBlank()) {
+        String name = txfName.getText();
+        if(name.isEmpty()){
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setHeaderText("Fejl");
             alert.setContentText("Kategorien skal have en beskrivelse");
             alert.showAndWait();
+        } else{
+            Controller.createCategory(name);
+            this.close();
         }
-        if (tfMortgage.isDisabled()) {
-            category = Controller.createCategory(name);
-        } else {
-            int mortgage;
-            try {
-                mortgage = Integer.parseInt(tfMortgage.getText());
-            } catch (NumberFormatException ignore) {
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setHeaderText("Fejl");
-                alert.setContentText("Pant skal være et tal");
-                alert.showAndWait();
-                return;
-            }
-            category = Controller.createRentalCategory(name,mortgage);
-        }
-        this.close();
-    }
-
-    public Category getCategory() {
-        return category;
     }
 }
--- src/Gui/Administration/Product/AddGiftBasketWindow.java
+++ src/Gui/Administration/Product/AddGiftBasketWindow.java
@@ -2,8 +2,9 @@ package Gui.Administration.Product;
 
 import Controller.Controller;
 import Gui.Observer;
-import Model.*;
-import javafx.geometry.HPos;
+import Model.Arrangement;
+import Model.Category;
+import Model.ProductComponent;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -15,10 +16,6 @@ import javafx.stage.StageStyle;
 import java.util.ArrayList;
 
 public class AddGiftBasketWindow extends Stage implements Observer {
-    private GiftBasket product;
-    private final ArrayList<TextField> alPrices = new ArrayList<>();
-    private final ArrayList<TextField> alClips = new ArrayList<>();
-    private int index = 0;
 
     private final TextField txfName = new TextField();
     private final TextField txfPrice = new TextField();
@@ -27,9 +24,6 @@ public class AddGiftBasketWindow extends Stage implements Observer {
     private final TextField txfSecondPrice = new TextField();
     private final ComboBox<Arrangement> cbArrangementsSecondPrice = new ComboBox<>();
     private final ComboBox<ProductComponent> cbProducts = new ComboBox<>();
-    private final Button btnCancel = new Button("Annuller");
-    private final Button btnOK = new Button("Bekræft");
-
 
     public AddGiftBasketWindow() {
         Controller.addObserver(this);
@@ -65,83 +59,66 @@ public class AddGiftBasketWindow extends Stage implements Observer {
         cbArrangements.setPromptText("Salgssituation");
 
         pane.add(btnAddPrice, 2, 3);
-        btnAddPrice.setOnAction(event -> addPriceAction(pane,null));
+        btnAddPrice.setOnAction(event -> addPriceAction(pane));
 
         Label lblProducts = new Label("Produkter");
         pane.add(lblProducts, 1, 4);
         pane.add(cbProducts, 2, 4);
 
+        Button btnCancel = new Button("Annuller");
         pane.add(btnCancel, 3, 6);
         btnCancel.setOnAction(event -> cancelAction());
 
+        Button btnOK = new Button("Bekræft");
         pane.add(btnOK, 4, 6);
         btnOK.setOnAction(event -> okAction());
     }
 
-    private void addPriceAction(GridPane pane, Price price) {
-        TextField tfPrice = new TextField();
-        TextField tfClip = new TextField();
-
-        pane.add(tfPrice, 1,index+3);
-        pane.add(tfClip, 2,index+3);
-
-        alPrices.add(tfPrice);
-        alClips.add(tfClip);
-
-        if(price != null){
-            tfPrice.setText(price.getPrice() + "");
-            tfClip.setText(price.getClips() + "");
-            cbArrangements.getSelectionModel().select(price.getArrangement());
-        }
-
-        pane.getChildren().removeAll(btnAddPrice,btnCancel,btnOK);
-
-        pane.add(btnAddPrice, 1, index+4);
-        pane.add(btnCancel, 2, index+5);
-        pane.add(btnOK, 3, index+5);
-
-        GridPane.setHalignment(btnCancel, HPos.RIGHT);
-        GridPane.setHalignment(btnOK, HPos.RIGHT);
-
-        this.sizeToScene();
-        index++;
+    private void addPriceAction(GridPane pane) {
+        Label lblPrice = new Label("Pris");
+        pane.add(lblPrice, 1, 3);
+        pane.add(txfSecondPrice, 2, 3);
+        pane.add(cbArrangementsSecondPrice, 3, 3, 2, 1);
+        cbArrangementsSecondPrice.getItems().addAll(Controller.getArrangements());
+        cbArrangementsSecondPrice.setPromptText("Salgssituation");
+        btnAddPrice.setVisible(false);
     }
 
     private void okAction() {
-        Category category = null;
-        for (Category category1 : Controller.getCategories()) {
-            if (category1.getName().equals("Sampakning")) {
-                category = category1;
-            }
-        }
-        if (category == null) {
-            category = Controller.createCategory("Sampakning");
-        }
-
         String productName = txfName.getText();
-        if (productName.isBlank()) {
+        String priceFromTextField = txfPrice.getText();
+        Arrangement arrangement = cbArrangements.getSelectionModel().getSelectedItem();
+        String secondPriceFromTextField = txfSecondPrice.getText();
+        Arrangement arrangementSecondPrice = cbArrangementsSecondPrice.getSelectionModel().getSelectedItem();
+        double price;
+
+        try {
+            price = Double.parseDouble(priceFromTextField);
+        } catch (NumberFormatException ex) {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setHeaderText("Fejl");
-            alert.setContentText("Alle felterne skal udfyldes");
+            alert.setContentText("Prisen skal være et tal");
             alert.showAndWait();
             return;
         }
 
-        GiftBasket giftBasket = Controller.createGiftBasket(productName, category, new ArrayList<>());
-/*
-        for (int i = 0; i < index; i++) {
-            Arrangement arrangement = alArrangements.get(i).getSelectionModel().getSelectedItem();
-            if (arrangement == null) continue;
-            try {
-                double price = Double.parseDouble(alPrices.get(i).getText());
-                Integer clips = Integer.valueOf(alClips.get(i).getText());
-                Controller.createPrice(giftBasket, arrangement,price,clips);
-            } catch (NumberFormatException e) {
-                System.out.println(e);
+        if (productName.isEmpty() || priceFromTextField.isEmpty() || arrangement == null) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText("Fejl");
+            alert.setContentText("Alle felterne skal udfyldes");
+            alert.showAndWait();
+        } else {
+            Category category = null;
+            for(Category cat : Controller.getCategories()){
+                if(cat.getName().equals("Sampakning")){
+                    category = cat;
+                }
             }
+            ArrayList<ProductComponent> products = new ArrayList<>();
+            ProductComponent newGiftBasket = Controller.createGiftBasket(productName, category, products);
+            Controller.createProductPrice(newGiftBasket, arrangement, price,null);
+            secondPriceAdded(secondPriceFromTextField, arrangementSecondPrice, newGiftBasket);
         }
-
- */
 
         this.close();
     }
@@ -161,7 +138,7 @@ public class AddGiftBasketWindow extends Stage implements Observer {
                 alert.showAndWait();
                 return;
             }
-            //Controller.createPrice(product, arrangementSecondPrice, secondPrice);
+            Controller.createProductPrice(product, arrangementSecondPrice, secondPrice,null);
         }
     }
 
